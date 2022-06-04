b3d4738bff3ec6d582a9a3a8f7751249779adb16
Ethan07902050 <b07902050@ntu.edu.tw>
2022-06-04 12:07:21 +0800

    save final result of cross validation in json

--- s3prl/downstream/mustard/gather_score.py
+++ s3prl/downstream/mustard/gather_score.py
@@ -1,4 +1,5 @@
 import jsonlines
+import json
 import argparse
 import numpy as np
 from pathlib import Path
@@ -7,7 +8,6 @@ def collect_metrics(expname):
     fold_list = []
     for i in range(5):
         fold = f'{expname}-fold-{i}'
-        root_dir = Path('result')
         metrics_path = f'result/downstream/{fold}/metrics.jsonl'
     
         with jsonlines.open(metrics_path, 'r') as reader:
@@ -17,7 +17,7 @@ def collect_metrics(expname):
     return fold_list
 
 
-def cv_score(fold_list):
+def cv_score(fold_list, expname):
     best_step, best_precision, best_recall, best_f1 = 0, 0, 0, 0
     for i in range(len(fold_list[0])):
         step = fold_list[0][i]['step']
@@ -31,8 +31,11 @@ def cv_score(fold_list):
             best_recall = recall
             best_step = step
         
+    result_dict = {'step': best_step, 'f1': best_f1, 'precision': best_precision, 'recall': best_recall}
     print('Best Score')
-    print(f'step: {best_step}, f1: {best_f1}, precision: {best_precision}, recall: {best_recall}')
+    print(result_dict)
+    with open(f'result/downstream/{expname}-metrics.json', 'w') as f:
+        json.dump(result_dict, f)
 
 
 def main():
@@ -41,7 +44,7 @@ def main():
     args = parser.parse_args()
 
     fold_list = collect_metrics(args.expname)
-    cv_score(fold_list)
+    cv_score(fold_list, args.expname)
 
 
 if __name__ == '__main__':
