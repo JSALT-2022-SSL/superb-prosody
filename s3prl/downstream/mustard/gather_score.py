810c5b0822f7038e72a10237c80d62ad496270a0
RT <yuantseng@gmail.com>
2022-06-25 22:35:08 +0800

    update config & better gather score path handling

--- s3prl/downstream/mustard/gather_score.py
+++ s3prl/downstream/mustard/gather_score.py
@@ -1,14 +1,15 @@
 import jsonlines
 import json
 import argparse
+import os
 import numpy as np
 from pathlib import Path
 
-def collect_metrics(expname):
+def collect_metrics(expdir):
     fold_list = []
     for i in range(5):
-        fold = f'{expname}-fold-{i}'
-        metrics_path = f'result/downstream/{fold}/metrics.jsonl'
+        fold = f'fold-{i}'
+        metrics_path = os.path.join(expdir, f'{fold}/metrics.jsonl')
     
         with jsonlines.open(metrics_path, 'r') as reader:
             metric_list = [obj for obj in reader]
@@ -17,7 +18,7 @@ def collect_metrics(expname):
     return fold_list
 
 
-def cv_score(fold_list, expname):
+def cv_score(fold_list, expdir):
     best_step, best_precision, best_recall, best_f1 = 0, 0, 0, 0
     for i in range(len(fold_list[0])):
         step = fold_list[0][i]['step']
@@ -34,17 +35,20 @@ def cv_score(fold_list, expname):
     result_dict = {'step': best_step, 'f1': best_f1, 'precision': best_precision, 'recall': best_recall}
     print('Best Score')
     print(result_dict)
-    with open(f'result/downstream/{expname}-metrics.json', 'w') as f:
+    with open(os.path.join(expdir, 'metrics.json'), 'w') as f:
         json.dump(result_dict, f)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--expname', help='Save experiment at result/downstream/expname')
+    parser.add_argument('-p', '--expdir', help='Save experiment at expdir')
     args = parser.parse_args()
 
-    fold_list = collect_metrics(args.expname)
-    cv_score(fold_list, args.expname)
+    #if args.expdir is None:
+    #    args.expdir = f'result/downstream/{args.expname}'
+
+    fold_list = collect_metrics(args.expdir)
+    cv_score(fold_list, args.expdir)
 
 
 if __name__ == '__main__':
--- s3prl/downstream/mustard/run_speaker_dependent.sh
+++ s3prl/downstream/mustard/run_speaker_dependent.sh
@@ -1,11 +1,13 @@
-EXP_NAME=mustard-hubert
-
-cd ../..
-for i in {0..4}
+upstream=$1
+EXP_NAME="mustard-$upstream"
+for lr in {1e-5,1e-4,1e-3}
 do
-  python run_downstream.py \
-    -m train -u hubert -d mustard \
-    -n $EXP_NAME-fold-$i \
-    -o "config.downstream_expert.datarc.speaker_dependent=True,,config.downstream_expert.datarc.split_no=${i}"
+  for i in {0..4}
+  do
+    python run_downstream.py \
+      -m train -u $upstream -d mustard \
+      -p result/$EXP_NAME-$lr/fold-$i \
+      -o "config.downstream_expert.datarc.speaker_dependent=True,,config.downstream_expert.datarc.split_no=${i},,config.optimizer.lr=$lr"
+  done
+  python downstream/mustard/gather_score.py -p result/$EXP_NAME-$lr
 done
-python downstream/mustard/gather_score.py -n $EXP_NAME
